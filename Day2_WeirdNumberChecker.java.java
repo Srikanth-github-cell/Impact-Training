a08d164ad056430051448e87b64ad01810f3d2a4
Srikanth Venkata <152138582+Srikanth-github-cell@users.noreply.github.com>
2025-05-09 18:45:59 +0530

    Day2_WeirdNumberChecker.java.java

--- Day2_WeirdNumberChecker.java.java
+++ Day2_WeirdNumberChecker.java.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class WeirdNumberChecker.java {
+public class WeirdNumberChecker {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
