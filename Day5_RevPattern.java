6856467f2b618bb255cdd89d4ecada422b315ec8
Srikanth Venkata <152138582+Srikanth-github-cell@users.noreply.github.com>
2025-05-09 18:59:38 +0530

    Update and rename nested_loop.java to Day5_RevPattern.java

--- Day5_RevPattern.java
+++ Day5_RevPattern.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class nested_loop {
+public class RevPattern {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
