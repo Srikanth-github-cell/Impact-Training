284571b20651199f798e9ba97085385b27f21fbe
Srikanth Venkata <152138582+Srikanth-github-cell@users.noreply.github.com>
2025-05-09 18:45:02 +0530

    Update Day1_SumFirstLastDigit.java

--- Day1_SumFirstLastDigit.java
+++ Day1_SumFirstLastDigit.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SumFirstLastDigit.java {
+public class SumFirstLastDigit {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
          System.out.println("enter a number:");
