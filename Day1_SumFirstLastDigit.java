4b5169b0f655ad19f989b40261ee809fd6793c3a
Srikanth Venkata <152138582+Srikanth-github-cell@users.noreply.github.com>
2025-05-09 18:34:11 +0530

    Update and rename ClassRoom.java to Day1_SumFirstLastDigit.java

--- Day1_SumFirstLastDigit.java
+++ Day1_SumFirstLastDigit.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ClassRoom {
+public class SumFirstLastDigit.java {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
          System.out.println("enter a number:");
